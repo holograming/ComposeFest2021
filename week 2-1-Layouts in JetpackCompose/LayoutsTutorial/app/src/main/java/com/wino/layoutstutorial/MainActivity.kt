f5cb5ef64140e26db0cd719dd803dd81eb841090
Essspressso <hologram1220@gmail.com>
2021-11-05 00:15:49 +0900

    feat: add simplelist tutorial

--- week 2-1-Layouts in JetpackCompose/LayoutsTutorial/app/src/main/java/com/wino/layoutstutorial/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsTutorial/app/src/main/java/com/wino/layoutstutorial/MainActivity.kt
@@ -3,11 +3,23 @@ package com.wino.layoutstutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 
 import com.wino.layoutstutorial.ui.theme.LayoutsTutorialTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +28,7 @@ class MainActivity : ComponentActivity() {
             LayoutsTutorialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+                    SimpleList()
                 }
             }
         }
@@ -26,6 +39,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LayoutsCodelabPreview() {
     LayoutsTutorialTheme {
-        
+        SimpleList()
     }
 }
