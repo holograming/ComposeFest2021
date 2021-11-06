6a3ee4476167089a5ee92c1e37aa6ae5acb6c614
Essspressso <hologram1220@gmail.com>
2021-11-06 21:20:37 +0900

    constraint layout

--- week 2-1-Layouts in JetpackCompose/LayoutsTutorial/app/src/main/java/com/wino/layoutstutorial/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsTutorial/app/src/main/java/com/wino/layoutstutorial/MainActivity.kt
@@ -10,14 +10,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.wino.layoutstutorial.ui.theme.LayoutsTutorialTheme
-import java.lang.reflect.Modifier
-import java.nio.file.Files.size
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,11 +35,53 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+        // Creates references for the three composables
+        // in the ConstraintLayout's body
+        val (button1, button2, text) = createRefs()
+
+        Button(
+            onClick = { /* Do something */ },
+            modifier = Modifier.constrainAs(button1) {
+                top.linkTo(parent.top, margin = 16.dp)
+            }
+        ) {
+            Text("Button 1")
+        }
+
+        Text("Text", Modifier.constrainAs(text) {
+            top.linkTo(button1.bottom, margin = 16.dp)
+            centerAround(button1.end)
+        })
+
+        val barrier = createEndBarrier(button1, text)
+        Button(
+            onClick = { /* Do something */ },
+            modifier = Modifier.constrainAs(button2) {
+                top.linkTo(parent.top, margin = 16.dp)
+                start.linkTo(barrier)
+            }
+        ) {
+            Text("Button 2")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ConstraintLayoutContentPreview() {
+    LayoutsTutorialTheme {
+        ConstraintLayoutContent()
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun LayoutsCodelabPreview() {
     LayoutsTutorialTheme {
-        LayoutsCodelabScaffold()
+        //LayoutsCodelabScaffold()
     }
 }
